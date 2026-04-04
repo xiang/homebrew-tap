ad87cdc3e9eafb4fd5d9b06db984263bcc863849
Sean Fortner <seanjfortner@gmail.com>
2026-04-04 17:22:29 +0200

    Update wg-widget to 1.0.0 (latest)

--- Casks/wg-widget.rb
+++ Casks/wg-widget.rb
@@ -1,8 +1,8 @@
 cask "wg-widget" do
-  version "dev"
-  sha256 :no_check # This allows the DMG to be overwritten without Homebrew errors
+  version "1.0.0"
+  sha256 :no_check
 
-  url "https://github.com/xiang/wg-widget/releases/download/v#{version}/wg-widget.dmg"
+  url "https://github.com/xiang/wg-widget/releases/download/latest/wg-widget.dmg"
   name "wg-widget"
   desc "Simple macOS Menu Bar wrapper for wg-quick"
   homepage "https://github.com/xiang/wg-widget"
