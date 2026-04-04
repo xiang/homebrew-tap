9065e6e435de802ba5c11c40cfb018a8208d8bcf
Sean Fortner <seanjfortner@gmail.com>
2026-04-04 17:13:29 +0200

    initial commit

--- Casks/wg-widget.rb
+++ Casks/wg-widget.rb
@@ -1,16 +1,14 @@
 cask "wg-widget" do
   version "dev"
-  sha256 "738f133415bddb1106bbc5f6bcff468b3600f242e7812486b45fa7e411fadacb"
+  sha256 :no_check # This allows the DMG to be overwritten without Homebrew errors
 
   url "https://github.com/xiang/wg-widget/releases/download/v#{version}/wg-widget.dmg"
   name "wg-widget"
-  desc "Vibe-coded Menu Bar wrapper for wg-quick"
+  desc "Simple macOS Menu Bar wrapper for wg-quick"
   homepage "https://github.com/xiang/wg-widget"
 
   depends_on formula: "wireguard-tools"
   depends_on formula: "bash"
 
   app "wg-widget.app"
-
-  zap trash: "~/.wireguard"
 end
---
+++ README.md
@@ -0,0 +1,13 @@
+# xiang/homebrew-tap
+
+Personal Homebrew Tap for custom macOS tools.
+
+## 📦 Available Casks
+
+### `wg-widget`
+A simple macOS Menu Bar wrapper for `wg-quick`.
+
+#### Installation
+```bash
+brew install --cask xiang/tap/wg-widget
+```
