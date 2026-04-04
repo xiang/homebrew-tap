c189816b4230b60a254f53fb736faff7675f76e9
Sean Fortner <seanjfortner@gmail.com>
2026-04-04 18:13:37 +0200

    wg-widget dev build (0.0.0-dev+6525ff9) [dev tag]

--- Casks/wg-widget.rb
+++ Casks/wg-widget.rb
@@ -2,7 +2,7 @@ cask "wg-widget" do
   version "0.0.0-dev+6525ff9"
   sha256 :no_check
 
-  url "https://github.com/xiang/wg-widget/releases/download/latest/wg-widget.dmg"
+  url "https://github.com/xiang/wg-widget/releases/download/dev/wg-widget.dmg"
   name "wg-widget"
   desc "Simple macOS Menu Bar wrapper for wg-quick"
   homepage "https://github.com/xiang/wg-widget"
