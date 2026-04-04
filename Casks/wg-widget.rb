ee3af1f74ead538b85e2bb1b25ddcd75cbcec25a
Sean Fortner <seanjfortner@gmail.com>
2026-04-04 18:53:32 +0200

    wg-widget dev build (0.0.0-dev+6525ff9) [dev tag]

--- Casks/wg-widget.rb
+++ Casks/wg-widget.rb
@@ -1,5 +1,5 @@
 cask "wg-widget" do
-  version "0.0.0-dev+6525ff9"
+  version "0.0.0-dev+6525ff9" # This will be overwritten by release.sh
   sha256 :no_check
 
   url "https://github.com/xiang/wg-widget/releases/download/dev/wg-widget.dmg"
